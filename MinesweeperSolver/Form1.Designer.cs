fc40fe5ec1b87a8816a887d28786d2327168d13a
Giovanni Mounir <giovannimounir@gmail.com>
2020-04-25 22:15:19 +0200

    Add fullscreen overlay, periodic screenshot
    - The form is now an overlay that fills the whole screen boundaries
    - A screenshot is taken every 1s

--- MinesweeperSolver/Form1.Designer.cs
+++ MinesweeperSolver/Form1.Designer.cs
@@ -29,12 +29,32 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(540, 364);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Minesweeper Solver";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- MinesweeperSolver/Form1.cs
+++ MinesweeperSolver/Form1.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Imaging;
 using System.Windows.Forms;
 
 namespace MinesweeperSolver
@@ -15,6 +11,37 @@ namespace MinesweeperSolver
         public Form1()
         {
             InitializeComponent();
+        }
+        private Rectangle ScreenBounds()
+        {
+            return Screen.GetBounds(Point.Empty);
+        }
+        private Bitmap CaptureScreen()
+        {
+            Rectangle bounds = ScreenBounds();
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                }
+                return bitmap;
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Rectangle bounds = ScreenBounds();
+            this.Location = new Point(0, 0);
+            this.Size = new Size(bounds.Width, bounds.Height);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            using (Bitmap screen = CaptureScreen())
+            {
+                //dosmth with screen
+            }
         }
     }
 }
