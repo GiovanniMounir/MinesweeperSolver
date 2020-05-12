87d0eb43e6684da45ab3dcf674c4f482f4ab47cc
Giovanni Mounir <giovannimounir@gmail.com>
2020-05-12 17:22:46 +0200

    Add copyright notice
    Co-Authored-By: polarag <polarag@users.noreply.github.com>
    Co-Authored-By: ahmedantaar <ahmedantaar@users.noreply.github.com>
    Co-Authored-By: Ahmed Atef <ahmedatef935@gmail.com>
    Co-Authored-By: Beshoy Abd Elsayed <beshoabdo8@gmail.com>

--- MinesweeperSolver/Form1.cs
+++ MinesweeperSolver/Form1.cs
@@ -1,4 +1,12 @@
-﻿using Emgu.CV;
+﻿/* Copyright 2020. Developed by Giovanni Mounir, Paul Ragheb, Beshoy Abd Elsayed, Ahmed Atef, Ahmed Anter
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+using Emgu.CV;
 using Emgu.CV.Structure;
 using Emgu.CV.Util;
 using System;
@@ -24,8 +32,8 @@ namespace MinesweeperSolver
         List<Rectangle> red_overlays = new List<Rectangle>();
         Dictionary<Color, int> numberMap = new Dictionary<Color, int>();
         /* ------------------------------------------ */
-        /* Import user32.dll to simulate mouse clicks */
-        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+/* Import user32.dll to simulate mouse clicks */
+[DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
 
         [DllImport("user32.dll")]
